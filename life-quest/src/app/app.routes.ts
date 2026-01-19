0b1d2349c51efc90e721d3838f44f9e4d53be1da
anissaCh13 <75808565+anissaCh13@users.noreply.github.com>
2026-01-19 10:35:26 +0100

    feat/life-24: Set up application routing with lazy-loaded pages
    * feat/life-24: Set up application routing with lazy-loaded pages
    
    * feat/life-24: Set up application routing with lazy-loaded pages (review)

--- life-quest/src/app/app.routes.ts
+++ life-quest/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.page').then((c) => c.DashboardPage),
+  },
+  {
+    path: 'goals',
+    loadComponent: () => import('./features/goals/goals.page').then((c) => c.GoalsPage)
+  },
+  {
+    path: 'goal/:goalId',
+    loadComponent: () => import('./features/goal-detail/goal-detail.page').then((c) => c.GoalDetailPage)
+  }
+];
---
+++ life-quest/src/app/features/goal-detail/goal-detail.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-detail.page',
+  imports: [],
+  templateUrl: './goal-detail.page.html',
+  styleUrl: './goal-detail.page.css',
+})
+export class GoalDetailPage {
+
+}
