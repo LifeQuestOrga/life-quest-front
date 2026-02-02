81fab52a4a2a1adfb6556595e50c9870a846c080
AnissaCh <anissa.ch.cours@gmail.com>
2026-02-02 10:26:10 +0100

    feat/life-25: Create the main layout component with mobile-first responsive design  + Implement footer component with responsive design

--- life-quest/src/app/shared/layout/footer/footer.ts
+++ life-quest/src/app/shared/layout/footer/footer.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Home, LucideAngularModule, Target } from 'lucide-angular';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.html',
-  standalone: true
+  imports: [
+    LucideAngularModule,
+    RouterLinkActive,
+    RouterLink
+  ],
+  standalone: true,
+  styleUrl: './footer.css',
 })
 export class Footer {
 
+  protected readonly Home = Home;
+  protected readonly Target = Target;
 }
